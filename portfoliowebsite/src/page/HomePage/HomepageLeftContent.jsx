b6c11198857f08e42d74f7285d0e581fb61d5254
AnsonL1234 <ansontan1001@gmail.com>
2025-07-19 12:21:31 +0800

    adding more design to homepage

--- portfoliowebsite/src/page/HomePage/HomepageLeftContent.jsx
+++ portfoliowebsite/src/page/HomePage/HomepageLeftContent.jsx
@@ -6,13 +6,13 @@ const HomepageLeftContent = () => {
         <div className="homepage-left-content">
             <h2>Welcome to my portfolio web page!</h2>
             <article>
-                My name is Anson Ling Guang Cheng. I'm a third years <a href="http://">business computing</a> student at TUDublin. If you wish to know more about me you can use the search bar and link section below find more about me
+                My name is Anson Ling Guang Cheng. I'm a third years <a href="http://" className='link-text'>Business Computing</a> student at <a href="https://www.tudublin.ie/" className='link-text'>TUDublin</a>. If you want to know more about me you can use the search bar or click on the link below for more info!
             </article>
             <form action="">
                 <input type="text" placeholder='enter the title and search whatever you like...' />
             </form>
             <div className="link-section">
-                <p>Link: <a href="http://">Developer</a> or Find me on: </p>
+                <p>Link: <a href="http://" className='link-text'>Developer</a> or Find me on: </p>
             </div>
         </div>
     );
